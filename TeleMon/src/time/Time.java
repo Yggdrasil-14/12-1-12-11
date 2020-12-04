67dfa4dc74176ccfaffd17e217947bb3faced6c5
DaikiHirohashi <75294107+DaikiHirohashi@users.noreply.github.com>
2020-12-04 16:44:17 +0900

    Add files via upload

--- TeleMon/src/time/Time.java
+++ TeleMon/src/time/Time.java
@@ -1,16 +1,14 @@
 package time;
 
-import java.time.LocalTime;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 public class Time {
-	private LocalDateTime nowDateTime,previousDateTime;
-	private int year,month,date,hour,minute,second;
+	private LocalDateTime nowDateTime,previousDateTime,previousEndDateTime;
 	
 	//コンストラクタ
-	Time(LocalDateTime recordDate){
-		previousDateTime=recordDate;
+	public Time(int data[]){
+		previousEndDateTime=LocalDateTime.of(data[0],data[1],data[2],data[3],data[4],data[5]);
 		nowDateTime=LocalDateTime.now();
 	}
 	
@@ -20,20 +18,45 @@ public class Time {
 		nowDateTime=LocalDateTime.now();
 	}
 	
-	//時間の差分算出
-	public int getDifferenceValue() {
-		long differenceValue;
-		differenceValue=ChronoUnit.SECONDS.between(previousDateTime,nowDateTime);
-		return (int)differenceValue;
+	//前回計測時間と現在時間の差分を算出
+	public int getHourBetweenPreviousNow() {
+		return (int)getHourBetweenArgumentNow(previousDateTime);
 	}
-	public int getDifferenceValue(LocalDateTime start,LocalDateTime end) {
+	
+	//前回終了時間と現在時間の差分を算出
+	public int getHourBetweenEndNow() {
+		return (int)getHourBetweenArgumentNow(previousEndDateTime);
+	}
+	
+	//引数の時間と現在時間の差分を算出
+	public long getHourBetweenArgumentNow(LocalDateTime start) {
 		long differenceValue;
-		differenceValue=ChronoUnit.SECONDS.between(start,end);
-		return (int)differenceValue;
+		differenceValue=ChronoUnit.HOURS.between(start,LocalDateTime.now());
+		return differenceValue;
+	}
+	public long getSecondBetweenArgumentNow(LocalDateTime start) {
+		long differenceValue;
+		differenceValue=ChronoUnit.SECONDS.between(start,LocalDateTime.now());
+		return differenceValue;
 	}
 	
 	//値取得
-	public LocalDateTime getNowDateTime() {
-		return nowDateTime;
+	public int getNowYear() {
+		return nowDateTime.getYear();
+	}
+	public int getNowMonth() {
+		return nowDateTime.getMonthValue();
+	}
+	public int getNowDay() {
+		return nowDateTime.getDayOfMonth();
+	}
+	public int getNowHour() {
+		return nowDateTime.getHour();
+	}
+	public int getNowMinute() {
+		return nowDateTime.getMinute();
+	}
+	public int getNowSecond() {
+		return nowDateTime.getSecond();
 	}
 }
