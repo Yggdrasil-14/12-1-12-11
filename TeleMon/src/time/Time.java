f756714740f9a7f0af0960dba855a7a1c5676790
DaikiHirohashi <75294107+DaikiHirohashi@users.noreply.github.com>
2020-12-04 11:34:43 +0900

    Add files via upload

---
+++ TeleMon/src/time/Time.java
@@ -0,0 +1,39 @@
+package time;
+
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Time {
+	private LocalDateTime nowDateTime,previousDateTime;
+	private int year,month,date,hour,minute,second;
+	
+	//コンストラクタ
+	Time(LocalDateTime recordDate){
+		previousDateTime=recordDate;
+		nowDateTime=LocalDateTime.now();
+	}
+	
+	//値更新
+	public void updateDateTime() {
+		previousDateTime=nowDateTime;
+		nowDateTime=LocalDateTime.now();
+	}
+	
+	//時間の差分算出
+	public int getDifferenceValue() {
+		long differenceValue;
+		differenceValue=ChronoUnit.SECONDS.between(previousDateTime,nowDateTime);
+		return (int)differenceValue;
+	}
+	public int getDifferenceValue(LocalDateTime start,LocalDateTime end) {
+		long differenceValue;
+		differenceValue=ChronoUnit.SECONDS.between(start,end);
+		return (int)differenceValue;
+	}
+	
+	//値取得
+	public LocalDateTime getNowDateTime() {
+		return nowDateTime;
+	}
+}
