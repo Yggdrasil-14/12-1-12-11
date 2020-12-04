7ae955f5b68ef215ce2ae1a5618985db0b94aa8f
SatoTsukasa1224 <75294214+SatoTsukasa1224@users.noreply.github.com>
2020-12-04 14:20:59 +0900

    Add files via upload

--- TeleMon/src/library/LibraryDraw.java
+++ TeleMon/src/library/LibraryDraw.java
@@ -1,29 +1,61 @@
 package library;
 
-import java.awt.Color;
+import java.awt.AlphaComposite;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
 
+
+
 import javax.swing.JPanel;
+
+
+
 	public class LibraryDraw extends JPanel{
+		
+		///////////
+		//画像ロード
+		///////////
+		
+		
 
-		Image book = Toolkit.getDefaultToolkit().getImage("img/Library.png");
+		
+		Image mon1 = Toolkit.getDefaultToolkit().getImage("img/Mon1.png");					//モンスター
+		
+		Image question = Toolkit.getDefaultToolkit().getImage("img/UnKnown.png");			//はてな
+		
+		Image Mess_Win = Toolkit.getDefaultToolkit().getImage("img/Messagewindow.png");		//メッセージウィンドウ
+		
+		Image Mess_Win2 = Toolkit.getDefaultToolkit().getImage("img/Messagewindow.png");	//メッセージウィンドウ2
+		
 
-		Image mon1 = Toolkit.getDefaultToolkit().getImage("img/アルラウネ1.png");
-
-		Image question = Toolkit.getDefaultToolkit().getImage("img/UnKnown.png");
-
-		Color col = new Color(0,255,255,100);
-
+		
+		
 	public void paintComponent(Graphics g) {
 			super.paintComponent(g);
-
-
-			g.drawImage(book, 0, 0,480,620, this);
-			g.drawImage(mon1, 0, 50,480/2,620/2, this);
-			g.drawImage(question, 480-230, 120,200,200, this);
+						
+			///////////
+			//描画
+			///////////
+			Graphics2D g2 = (Graphics2D)g;
+			 
+			//透明度を50%に設定する
+			AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f);
+			g2.setComposite(ac);
+	 
+			//g2.drawImage(img, 0, 0, this);
+					
+			g2.drawImage(mon1, 0, 50,480/2,620/2, this);		//モンスター
+			
+			g2.drawImage(question, 275, 140,150,150, this);		//はてな
+			
+			g2.drawImage(Mess_Win, 18, 350,210,130, this);		//メッセージウィンドウ
+			
+			g2.drawImage(Mess_Win2, 480/2, 350,210,130, this);	//メッセージウィンドウ2
 
 		}
+	
+	
 	}
 
--- TeleMon/src/library/LibraryMain.java
+++ TeleMon/src/library/LibraryMain.java
@@ -1,18 +1,83 @@
-
-
 package library;
 
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
 
 
-public class LibraryMain extends JFrame{
 
-		public LibraryMain(String title, int width, int height) {
+//import jdk.internal.platform.Container;
+
+public class LibraryMain extends JFrame implements ActionListener{
+	final String back = "back";			//ページ戻る
+	final String forward = "forward";   //ページめくる
+	LibraryDraw LD;
+	LibraryBack LB;
+
+		public LibraryMain(String title, int width, int height) {			
 			super(title);
+			
 			setDefaultCloseOperation(EXIT_ON_CLOSE);
 			setSize(width,height);
 			setLocationRelativeTo(null);
+			
+			LD=new LibraryDraw();
+			LB=new LibraryBack();
+			LD.setBounds(0,0,480,620);
+			LB.setBounds(0,0,480,620);
+			LD.setOpaque(true);
+			Container contentPane = getContentPane();
+			
+			JLayeredPane pane = new JLayeredPane();
+
+			JButton btn1 = new JButton("←");
+			JButton btn2 = new JButton("→");
+			
+//			btn1.setBounds(60,570,80,30);
+//			btn2.setBounds(150,570,80,30);
+			
+			btn1.setActionCommand(back);
+			btn1.addActionListener(this);		//ボタンが押された
+			btn2.setActionCommand(forward);
+            btn2.addActionListener(this);
+			//contentPane.setLayout(null);		//自分で位置設定するならnullを入れる
+			
+//			contentPane.add(btn1);
+//			contentPane.add(btn2);
+//			contentPane.add(LD);
+            
+            btn1.setBounds(25,490,80,30);		//戻るボタン
+			btn2.setBounds(365,490,80,30);		//進むボタン
+
+			pane.add(btn1);
+			pane.add(btn2);
+
+			pane.setLayer(btn1, 50);
+			pane.setLayer(btn2, 51);
+			
+			pane.add(LD);
+			pane.setLayer(LD, 42);
+			pane.add(LB);
+			pane.setLayer(LB, 40);
+			contentPane.add(pane);			
+			
+		}
+		
+		public void actionPerformed(ActionEvent event){
+
+			String cmd = event.getActionCommand();
+			
+			if(cmd.equals(back)) {
+				System.out.println("back");		//ボタンが押されるとコンソールに文字表示
+				LD.setVisible(false);
+				}
+			else if(cmd.equals(forward)) {
+				System.out.println("forward");	//ボタンが押されるとコンソールに文字表示
+				}
 		}
 
 }
