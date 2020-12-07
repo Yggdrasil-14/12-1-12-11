a6b0bb856bf100795f57c93c70a20ad552aa94ac
SatoTsukasa1224 <75294214+SatoTsukasa1224@users.noreply.github.com>
2020-12-07 15:03:41 +0900

    Add files via upload

--- TeleMon/src/library/LibraryMain.java
+++ TeleMon/src/library/LibraryMain.java
@@ -8,48 +8,40 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
 
-
-
-//import jdk.internal.platform.Container;
-
 public class LibraryMain extends JFrame implements ActionListener{
 	final String back = "back";			//ページ戻る
 	final String forward = "forward";   //ページめくる
 	LibraryDraw LD;
-	//LibraryBack LB;
-
-		public LibraryMain(String title, int width, int height) {
+	
+		public LibraryMain(String title, int width, int height) {			
 			super(title);
-
+			
 			setDefaultCloseOperation(EXIT_ON_CLOSE);
 			setSize(width,height);
 			setLocationRelativeTo(null);
-
+			
 			LD=new LibraryDraw();
 			//LB=new LibraryBack();
 			LD.setBounds(0,0,480,620);
 			//LB.setBounds(0,0,480,620);
 			LD.setOpaque(true);
 			Container contentPane = getContentPane();
-
+			
 			JLayeredPane pane = new JLayeredPane();
 
-			JButton btn1 = new JButton("←");
-			JButton btn2 = new JButton("→");
+			JButton btn1 = new JButton("←");	//戻るボタン
+			JButton btn2 = new JButton("→");	//進むボタン
+			
 
-//			btn1.setBounds(60,570,80,30);
-//			btn2.setBounds(150,570,80,30);
-
+			
 			btn1.setActionCommand(back);
 			btn1.addActionListener(this);		//ボタンが押された
 			btn2.setActionCommand(forward);
             btn2.addActionListener(this);
 			//contentPane.setLayout(null);		//自分で位置設定するならnullを入れる
+			
 
-//			contentPane.add(btn1);
-//			contentPane.add(btn2);
-//			contentPane.add(LD);
-
+            
             btn1.setBounds(25,490,80,30);		//戻るボタン
 			btn2.setBounds(365,490,80,30);		//進むボタン
 
@@ -58,25 +50,31 @@ public class LibraryMain extends JFrame implements ActionListener{
 
 			pane.setLayer(btn1, 50);
 			pane.setLayer(btn2, 51);
-
+			
 			pane.add(LD);
 			pane.setLayer(LD, 42);
 			//pane.add(LB);
 			//pane.setLayer(LB, 40);
-			contentPane.add(pane);
-
+			contentPane.add(pane);			
+			
 		}
-
+		
 		public void actionPerformed(ActionEvent event){
 
 			String cmd = event.getActionCommand();
-
-			if(cmd.equals(back)) {
-				System.out.println("back");		//ボタンが押されるとコンソールに文字表示
+			
+			/*ページ切り替え*/	
+			if(cmd.equals(back)) {					//戻るボタンクリックでLD.pageをデクリメント
 				LD.setVisible(false);
+				if(LD.page>0) {LD.page--;}
+				System.out.println(LD.page);		//ボタンが押されるとコンソールに文字表示
+				LD.setVisible(true);
 				}
-			else if(cmd.equals(forward)) {
-				System.out.println("forward");	//ボタンが押されるとコンソールに文字表示
+			else if(cmd.equals(forward)) {			//進むボタンクリックでLD.pageをインクリメント
+				LD.setVisible(false);
+				if(LD.page<2) {LD.page++;}
+				System.out.println(LD.page);		//ボタンが押されるとコンソールに文字表示
+				LD.setVisible(true);
 				}
 		}
 
