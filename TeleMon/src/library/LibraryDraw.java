d1622a34a0a3be071926bbb7759d68124cae1f12
SatoTsukasa1224 <75294214+SatoTsukasa1224@users.noreply.github.com>
2020-12-10 11:45:50 +0900

    Add files via upload

--- TeleMon/src/library/LibraryDraw.java
+++ TeleMon/src/library/LibraryDraw.java
@@ -13,8 +13,16 @@ import data.Save;
 	public class LibraryDraw extends JPanel{
 
 		Character Mon[] = new Character[Save.cl];
-
-		Image img[] = new Image[Save.cl];
+		
+		Image N[]=new Image[13]; 
+		Image Mon_Ex[]=new Image[Save.cl]; 
+		Image MonImg[] = new Image[Save.cl];
+		int[] FriendShip=new int[Save.cl];
+		Image Heart[] = new Image[5];
+		Image HeartSpace[] = new Image[5];
+		
+		int Comprate,Compcount;
+		
 		public LibraryDraw(Character c[]) {
 			String data[] = new String[Save.ec];
 			for(int i=0;i<Save.cl;i++) {
@@ -33,15 +41,48 @@ import data.Save;
 			data[12] = "0";
 			data[13] = "0";
 			
-			img[i] = Toolkit.getDefaultToolkit().getImage("src/character/Material/" + data[0] +".png");
+			MonImg[i] = Toolkit.getDefaultToolkit().getImage("src/character/Material/" + data[0] +".png");
+			
+			Mon_Ex[i] = Toolkit.getDefaultToolkit().getImage("img/Monster_Ex_"+(i+1)+".png");
 			
 			Mon[i] = new Character(data);
-
+			
+			if(Mon[i].getCapture() == 1) {
+				Compcount++;
+				System.out.println("data[1]:"+data[1]);
+				System.out.println("compcount:"+Compcount);
 			}
+			
+			FriendShip[i]=c[i].getFriendship();
+			
+			
+			}
+			System.out.println("compcount:"+Compcount);
+			System.out.println("Save.cl:"+Save.cl);
+			Comprate= (Compcount*100)/Save.cl;
+			System.out.println("comprate:"+Comprate);
+			
+			System.out.println("FriendShip:"+FriendShip[0]);
+
+
+			for(int i=0;i<10;i++) {
+			String s=Integer.toString(i);
+			N[i]= Toolkit.getDefaultToolkit().getImage("img/"+s+".png");			//ナンバー
+			//System.out.println("img/"+i+".png");
+			}
+			
+			
+			for(int i=0;i<5;i++){
+				Heart[i]= Toolkit.getDefaultToolkit().getImage("img/Heart.png");
+				HeartSpace[i]= Toolkit.getDefaultToolkit().getImage("img/HeartSpace.png");
+			}
+			
+			
 		}
 
 
-		int page;
+		 public static int page=0;
+		
 
 		///////////
 		//画像ロード
@@ -53,9 +94,16 @@ import data.Save;
 		Image Mess_win = Toolkit.getDefaultToolkit().getImage("img/Messagewindow.png");					//メッセージウィンドウ
 		Image Achive_re_T = Toolkit.getDefaultToolkit().getImage("img/Achievementreward_Title.png");	//達成報酬(タイトル)
 		Image Achive_re = Toolkit.getDefaultToolkit().getImage("img/Achievementreward.png");			//達成報酬
-		Image Mon_ex = Toolkit.getDefaultToolkit().getImage("img/ArlauneExplanation.png");				//モンスター説明テキスト
 		Image Comp_rate_T = Toolkit.getDefaultToolkit().getImage("img/Completerate_Title.png");			//コンプリート率(タイトル)
-
+		Image No = Toolkit.getDefaultToolkit().getImage("img/No.png");									//ナンバー（No.）
+		Image Percent = Toolkit.getDefaultToolkit().getImage("img/Percent.png");						//コンプリート率表示(%)
+		Image Comp_rate_line = Toolkit.getDefaultToolkit().getImage("img/Comprateunder.png");
+		Image Hanamaru = Toolkit.getDefaultToolkit().getImage("img/Hanamaru.png");						//はなまる
+		Image AllPolocks = Toolkit.getDefaultToolkit().getImage("img/AllPolocks.png");					//すべてのポロック
+		Image kakeru = Toolkit.getDefaultToolkit().getImage("img/×.png");								//達成報酬(×)
+				
+		//Image HeartSpace = Toolkit.getDefaultToolkit().getImage("img/HeartSpace.png");
+		
 
 	public void paintComponent(Graphics g) {
 			super.paintComponent(g);
@@ -63,34 +111,177 @@ import data.Save;
 			///////////
 			//描画
 			///////////
-
+			
+			int No_digit_1;								//図鑑ナンバー表示1桁目
+			int No_digit_2;								//図鑑ナンバー表示2桁目
+			
+			int Comp_digit_1;								//コンプリート率表示1桁目
+			int Comp_digit_2;								//コンプリート率表示2桁目
+			int Comp_digit_3;								//コンプリート率表示3桁目
+			
 			g.drawImage(Back, 0, 0,480,620, this);					//背景
-
-			g.drawImage(book, -3, 0,480,620, this);					//本
-
-
+			g.drawImage(book, 0, 0,480,620, this);					//本
+			
+			Comp_digit_3=Comprate/100;
+			Comp_digit_2=Comprate/10;
+			Comp_digit_1=Comprate-Comp_digit_2*10;
+				
 				if(page == 0) {
-				g.drawImage(Comp_rate_T, 40, 60,180,50, this);		//コンプリート率(タイトル)
-				g.drawImage(Achive_re_T, 260, 60,180,50, this);		//達成報酬(タイトル)
-				g.drawImage(Achive_re, 225, 120,300,360, this);		//達成報酬
+				g.drawImage(Comp_rate_T, 40, 60,180,50, this);			//コンプリート率(タイトル)
+				g.drawImage(Achive_re_T, 260, 60,180,50, this);			//達成報酬(タイトル)
+				g.drawImage(Achive_re, 225, 120,300,380, this);			//達成報酬
+				
+				g.drawImage(AllPolocks, 350, 120,70,70, this);			//報酬ポロック
+				g.drawImage(kakeru, 380, 183,35,35, this);				//達成報酬(×)
+				g.drawImage(N[2], 410, 187,30,30, this);				//報酬数
+				
+				g.drawImage(AllPolocks, 350, 220,70,70, this);			//報酬ポロック
+				g.drawImage(kakeru, 380, 283,35,35, this);				//達成報酬(×)
+				g.drawImage(N[5], 410, 287,30,30, this);				//報酬数
+				
+				g.drawImage(AllPolocks, 350, 320,70,70, this);			//報酬ポロック
+				g.drawImage(kakeru, 380, 383,35,35, this);				//達成報酬(×)
+				g.drawImage(N[7], 410, 387,30,30, this);				//報酬数
+				
+				g.drawImage(AllPolocks, 350, 420,70,70, this);			//報酬ポロック
+				g.drawImage(kakeru, 380, 483,35,35, this);				//達成報酬(×)
+				g.drawImage(N[1], 410, 487,30,30, this);				//報酬数
+				g.drawImage(N[0], 430, 487,30,30, this);				//報酬数
+				
+				if(Comprate!=100) {
+				g.drawImage(N[Comp_digit_1], 120, 240,40,60, this);		//コンプリート率表示1桁目	
+				if(Comp_digit_2!=0) {
+				g.drawImage(N[Comp_digit_2], 80, 240,40,60, this);		//コンプリート率表示2桁目
+				}
+				if(Comp_digit_3!=0) {
+				g.drawImage(N[Comp_digit_3], 40, 240,40,60, this);		//コンプリート率表示3桁目
+				}
+				}else if(Comprate==100) {
+					g.drawImage(N[0], 120, 240,40,60, this);			//コンプリート率表示1桁目	
+					g.drawImage(N[0], 80, 240,40,60, this);				//コンプリート率表示2桁目
+					g.drawImage(N[1], 40, 240,40,60, this);				//コンプリート率表示3桁目
+					g.drawImage(Hanamaru, 100, 100,150,150, this);		//はなまる
+				}
+				g.drawImage(Percent, 160, 240,40,60, this);				//コンプリート率表示(%)
+				g.drawImage(Comp_rate_line, 20, 290,220,50, this);		//コンプリート率のアンダーライン
+				
 				}
 				
 				else if(page != 0) {
-				if(Mon[2*(page-1)].getCapture() == 1) {
-				g.drawImage(img[2*(page-1)], 40, 90,480/3,620/3, this);}			//モンスター
-				else{g.drawImage(question, 40, 140,150,150, this);}//はてな
 				
-				g.drawImage(Mess_win, 18, 350,210,130, this);		//メッセージウィンドウ(左)
-				g.drawImage(Mon_ex, 30, 370,190,100, this);			//モンスター説明テキスト
-				g.drawImage(Mess_win, 480/2, 350,210,130, this);	//メッセージウィンドウ(右)
+				g.drawImage(Mess_win, 18, 350,215,130, this);							//メッセージウィンドウ(左)			
+				
+				if(Mon[2*(page-1)].getCapture() == 1) {
+				g.drawImage(MonImg[2*(page-1)], 50, 130,140,180, this);					//モンスター
+				g.drawImage(Mon_Ex[2*(page-1)], 30, 380,195,80, this);					//モンスター説明テキスト
+				
+				g.drawImage(HeartSpace[0], 20+110, 325,20,20, this);
+				g.drawImage(HeartSpace[1], 20*2+110, 325,20,20, this);
+				g.drawImage(HeartSpace[2], 20*3+110, 325,20,20, this);
+				g.drawImage(HeartSpace[3], 20*4+110, 325,20,20, this);
+				g.drawImage(HeartSpace[4], 20*5+110, 325,20,20, this);
+				
+				switch((FriendShip[2*(page-1)])/20) {
+				case 0:					
+					break;
+				case 1:
+					g.drawImage(Heart[0], 20+110, 325,20,20, this);
+					break;
+				case 2:
+					g.drawImage(Heart[0], 20+110, 325,20,20, this);
+					g.drawImage(Heart[1], 20*2+110, 325,20,20, this);
+					break;
+				case 3:
+					g.drawImage(Heart[0], 20+110, 325,20,20, this);
+					g.drawImage(Heart[1], 20*2+110, 325,20,20, this);
+					g.drawImage(Heart[2], 20*3+110, 325,20,20, this);
+					break;
+				case 4:
+					g.drawImage(Heart[0], 20+110, 325,20,20, this);
+					g.drawImage(Heart[1], 20*2+110, 325,20,20, this);
+					g.drawImage(Heart[2], 20*3+110, 325,20,20, this);
+					g.drawImage(Heart[3], 20*4+110, 325,20,20, this);
+					break;
+				case 5:
+					g.drawImage(Heart[0], 20+110, 325,20,20, this);
+					g.drawImage(Heart[1], 20*2+110, 325,20,20, this);
+					g.drawImage(Heart[2], 20*3+110, 325,20,20, this);
+					g.drawImage(Heart[3], 20*4+110, 325,20,20, this);
+					g.drawImage(Heart[4], 20*5+110, 325,20,20, this);
+				break;
+				}
+				
+				}				
+				else{g.drawImage(question, 40, 140,150,150, this);}						//はてな
+				
+					
+			    g.drawImage(No, 20, 70,60,30, this);									//ナンバー（No.）
+			    
+				No_digit_1=(2*(page-1)+1)%10;
+				No_digit_2=(2*(page-1)+1)/10;
+				g.drawImage(N[No_digit_1], 105, 70,35,35, this);						//図鑑ナンバー1桁目の数字	
+				g.drawImage(N[No_digit_2], 80, 70,35,35, this);							//図鑑ナンバー2桁目の数字
+				
 
+				
+				
+				g.drawImage(Mess_win, 480/2, 350,215,130, this);						//メッセージウィンドウ(右)
+				
 				if(Mon[2*(page-1)+1].getCapture() == 1) {
-					g.drawImage(img[2*(page-1)+1], 260, 90,480/3,620/3, this);}			//モンスター
-					else{g.drawImage(question, 275, 140,150,150, this);}//はてな
-				g.drawImage(Mess_win, 18, 350,210,130, this);		//メッセージウィンドウ(左)
-				//g.drawImage(Mon_ex, 30, 370,190,100, this);			//モンスター説明テキスト
-				g.drawImage(Mess_win, 480/2, 350,210,130, this);	//メッセージウィンドウ(右)
-			}
+				g.drawImage(MonImg[2*(page-1)+1], 270, 130,140,180, this);				//モンスター
+				g.drawImage(Mon_Ex[2*(page-1)+1], 250, 380,195,80, this);				//モンスター説明テキスト
+				
+				g.drawImage(HeartSpace[0], 20+330, 325,20,20, this);
+				g.drawImage(HeartSpace[1], 20*2+330, 325,20,20, this);
+				g.drawImage(HeartSpace[2], 20*3+330, 325,20,20, this);
+				g.drawImage(HeartSpace[3], 20*4+330, 325,20,20, this);
+				g.drawImage(HeartSpace[4], 20*5+330, 325,20,20, this);
+				
+				switch((FriendShip[2*(page-1)+1])/20) {
+				case 0:					
+					break;
+				case 1:
+					g.drawImage(Heart[0], 20+330, 325,20,20, this);
+					break;
+				case 2:
+					g.drawImage(Heart[0], 20+330, 325,20,20, this);
+					g.drawImage(Heart[1], 20*2+330, 325,20,20, this);
+					break;
+				case 3:
+					g.drawImage(Heart[0], 20+330, 325,20,20, this);
+					g.drawImage(Heart[1], 20*2+330, 325,20,20, this);
+					g.drawImage(Heart[2], 20*3+330, 325,20,20, this);
+					break;
+				case 4:
+					g.drawImage(Heart[0], 20+330, 325,20,20, this);
+					g.drawImage(Heart[1], 20*2+330, 325,20,20, this);
+					g.drawImage(Heart[2], 20*3+330, 325,20,20, this);
+					g.drawImage(Heart[3], 20*4+330, 325,20,20, this);
+					break;
+				case 5:
+					g.drawImage(Heart[0], 20+330, 325,20,20, this);
+					g.drawImage(Heart[1], 20*2+330, 325,20,20, this);
+					g.drawImage(Heart[2], 20*3+330, 325,20,20, this);
+					g.drawImage(Heart[3], 20*4+330, 325,20,20, this);
+					g.drawImage(Heart[4], 20*5+330, 325,20,20, this);
+				break;
+				}
+			
+				}
+				else{g.drawImage(question, 275, 140,150,150, this);					//はてな
+				}
+				
+				
+				g.drawImage(No, 250, 70,60,30, this);									//図鑑ナンバー（No.）
+				
+				No_digit_1=(2*(page-1)+2)%10;
+				No_digit_2=(2*(page-1)+2)/10;
+				g.drawImage(N[No_digit_1], 335, 70,35,35, this);						//図鑑ナンバー(1桁目の数字)	
+				g.drawImage(N[No_digit_2], 310, 70,35,35, this);						//図鑑ナンバー(2桁目の数字)
+					
+
+			
+				}
 
 			}
 		}
--- TeleMon/src/library/LibraryMain.java
+++ TeleMon/src/library/LibraryMain.java
@@ -15,7 +15,11 @@ public class LibraryMain extends JFrame implements ActionListener{
 	final String back = "back";			//ページ戻る
 	final String forward = "forward";//ページめくる
 	final String GoHome = "GoHome";
+	final String favoriteL = "favoriteL";
+	final String favoriteR = "favoriteR";
+	int FavoriteMonster;
 	LibraryDraw LD;
+	JButton btn4,btn5;
 
 		public LibraryMain(String title, int width, int height,Character[] ms) {
 			super(title);
@@ -35,7 +39,9 @@ public class LibraryMain extends JFrame implements ActionListener{
 
 			JButton btn1 = new JButton("←");	//戻るボタン
 			JButton btn2 = new JButton("→");	//進むボタン
-			JButton btn3 = new JButton("Home");
+			JButton btn3 = new JButton("ホーム");
+			btn4 = new JButton("★");
+			btn5 = new JButton("★");
 
 
 			btn1.setActionCommand(back);
@@ -44,21 +50,35 @@ public class LibraryMain extends JFrame implements ActionListener{
             btn2.addActionListener(this);
             btn3.setActionCommand(GoHome);
             btn3.addActionListener(this);
+            btn4.setActionCommand(favoriteL);
+            btn4.addActionListener(this);
+            btn5.setActionCommand(favoriteR);
+            btn5.addActionListener(this);
             //contentPane.setLayout(null);		//自分で位置設定するならnullを入れる
 
 
 
-            btn1.setBounds(25,490,80,30);		//戻るボタン
-			btn2.setBounds(365,490,80,30);		//進むボタン
-			btn3.setBounds(25,550,80,50);
+            btn1.setBounds(25,520,80,20);		//戻るボタン
+			btn2.setBounds(365,520,80,20);		//進むボタン
+			btn3.setBounds(25,550,80,30);
+			btn4.setBounds(25,315,80,30);
+			btn5.setBounds(245,315,80,30);
 
 			pane.add(btn1);
 			pane.add(btn2);
 			pane.add(btn3);
+			
+			
+			pane.add(btn4);
+			pane.add(btn5);
+			btn4.setVisible(false);
+			btn5.setVisible(false);
 
 			pane.setLayer(btn1, 50);
 			pane.setLayer(btn2, 51);
 			pane.setLayer(btn3, 51);
+			pane.setLayer(btn4, 51);
+			pane.setLayer(btn5, 51);
 
 			pane.add(LD);
 			pane.setLayer(LD, 42);
@@ -87,8 +107,73 @@ public class LibraryMain extends JFrame implements ActionListener{
 			else if(LD.page == 7){}
 			else if(cmd.equals(GoHome)) {
 				main.Main.OpenH();
-				home.Home.CloseL();
+				this.setVisible(false);
 					}
+			if(LD.page==0) {
+				btn4.setVisible(false);
+				btn5.setVisible(false);
+			}else {
+				btn4.setVisible(true);
+				btn5.setVisible(true);
+			}
+			
+			if(cmd.equals(favoriteL)) {					
+				LD.setVisible(false);
+				switch(2*(LD.page-1)) {
+				case 0:
+					FavoriteMonster=0;
+					break;
+				case 2:
+					FavoriteMonster=2;
+					break;
+				case 4:
+					FavoriteMonster=4;
+					break;
+				case 6:
+					FavoriteMonster=6;
+					break;
+				case 8:
+					FavoriteMonster=8;
+					break;
+				case 10:
+					FavoriteMonster=10;
+					break;
+				
+				}
+				
+				
+				System.out.println("FavoriteMonster:"+FavoriteMonster);		//お気に入りモンスター番号表示
+				LD.setVisible(true);
+				}
+			
+			if(cmd.equals(favoriteR)) {					//戻るボタンクリックでLD.pageをデクリメント
+				LD.setVisible(false);
+				switch(2*(LD.page-1)+1) {
+				case 1:
+					FavoriteMonster=1;
+					break;
+				case 3:
+					FavoriteMonster=3;
+					break;
+				case 5:
+					FavoriteMonster=5;
+					break;
+				case 7:
+					FavoriteMonster=7;
+					break;
+				case 9:
+					FavoriteMonster=9;
+					break;
+				case 11:
+					FavoriteMonster=11;
+					break;
+				
+				}
+				
+				
+				System.out.println("FavoriteMonster:"+FavoriteMonster);		//お気に入りモンスター番号表示
+				LD.setVisible(true);
+				}
 		}
 
 }
